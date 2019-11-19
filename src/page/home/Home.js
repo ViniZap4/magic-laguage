8fc6f5434d19c3d5733845d17aa0be72e57623bd
Vinicius Zapalá <vinizap@outlook.com>
2019-11-19 18:02:33 -0300

    estilizando home e menu

--- src/page/home/Home.js
+++ src/page/home/Home.js
@@ -16,8 +16,8 @@ export default function Home(){
           <h1> Quem somos </h1>
           
           <p className="open_content">
-            Somos uma plataforma de aprendizado da língua <br /> 
-            inglesa desenvolvida para facilitar <br />
+            Somos uma plataforma de aprendizado da língua  
+            inglesa desenvolvida para facilitar 
             o entendimento do idioma. 
           </p>
         </div>
@@ -25,15 +25,15 @@ export default function Home(){
           <h1> Como fazemos </h1>
           
           <p className="open_content">
-            Através de lições simples e práticas que demandam <br />
-            apenas 15 minutos de estudo por dia para  <br />
+            Através de lições simples e práticas que demandam 
+            apenas 15 minutos de estudo por dia para  
             aprimorar o aprendizado.
           </p>
         </div>
       </div>
             
       <div className="temes">
-        <div id="vocabulary_teme"   className="teme_vocabulary">
+        <div id="vocabulary_teme"   className="teme">
           <h1> Vocabulary Teme </h1>
 
           <ul className="open_content">
@@ -43,7 +43,7 @@ export default function Home(){
             <li>Day of the week</li>
           </ul>
         </div>
-        <div id="grammar_teme"  className="teme_grammar">
+        <div id="grammar_teme"  className="teme">
           <h1> Grammar Teme </h1>
 
           <ul className="open_content">    
